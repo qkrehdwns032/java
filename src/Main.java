90ef947ade8fab4e17100fb4bf13ac7348585c11
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2025-09-18 23:42:22 +0900

    10101

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Scanner sc = new Scanner(System.in);
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < N; i++){
-            int M = Integer.parseInt(br.readLine());
-
-            if(maxHeap.isEmpty() || M <= maxHeap.peek()){
-                maxHeap.add(M);
-            } else {
-                minHeap.add(M);
-            }
-
-            if(maxHeap.size() > minHeap.size() + 1){
-                minHeap.add(maxHeap.poll());
-            } else if(minHeap.size() > maxHeap.size()){
-                maxHeap.add(minHeap.poll());
-            }
-
-            bw.write(maxHeap.peek() + "\n");
+        if(a + b + c != 180){
+            System.out.print("Error");
         }
-
-        br.close();
-        bw.close();
+        else if(a + b + c == 180 && a != b && b != c && a != c){
+            System.out.println("Scalene");
+        }
+        else if(a + b + c == 180 && a == b && a != c || a == c && a != b || b ==c && b != a){
+            System.out.println("Isosceles");
+        }
+        else if(a == 60 && b == 60 && c == 60){
+            System.out.println("Equilateral");
+        }
     }
 }
